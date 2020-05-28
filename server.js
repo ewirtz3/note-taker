ed6f1e30bb2e9d27198cd0450cb84f6a4f171b67
Emily Wirtz <ewirtz3@gmail.com>
2020-05-27 21:14:23 -0500

    promisifying fs.readFile and fs.appendFile

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//setting up dependencies
 const fs = require("fs");
 const path = require("path");
-const util = require("util");
+const { promisify } = require("util");
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 
 //setting up Express and defining PORT
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//promisifying fs.readFile and fs.appendFile
+const readFileAsync = promisify(fs.readFile);
+const appendFileAsync = promisify(fs.appendFile);
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
@@ -30,6 +35,7 @@ app
   })
   .post(function (req, res) {
     const newNote = req.body;
+    newNote.id = uuidv4();
     fs.appendFile("db.json", newNote);
     return newNote;
   });
