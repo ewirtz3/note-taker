040a9830b5c258dabf282e909f12c6caeca364ff
Emily Wirtz <ewirtz3@gmail.com>
2020-05-30 14:36:20 -0500

    fixed it

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 3000;
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 
+//defining route
+const db_FILE = path.join(__dirname, "db", "db.json");
+
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-//defining data as an array
-let notesArray = [];
 
 //creating HTML routes
 app.get("/", function (req, res) {
@@ -33,30 +33,37 @@ app.get("/notes", function (req, res) {
 //creating API routes
 app.get("/api/notes", function (req, res) {
   console.log("this route is hit");
-  readFileAsync(path.join(__dirname, "db", "db.json"), "utf8").then((notes) => {
+  readFileAsync(db_FILE, "utf8").then((notes) => {
     console.log(JSON.parse(notes));
-    notesArray = JSON.parse(notes);
     return res.json(JSON.parse(notes));
   });
 });
 
 app.post("/api/notes", function (req, res) {
-  console.log("sure whatever", req.body);
-  const newNote = req.body;
-  newNote.id = uuidv4();
-  notesArray.push(newNote);
-  console.log(notesArray);
-  writeFileAsync(
-    path.join(__dirname, "db", "db.json"),
-    JSON.stringify(notesArray)
-  ).then(() => {
-    readFileAsync(path.join(__dirname, "db", "db.json"), "utf8").then(() => {
-      return res.json(JSON.parse(newNote));
+  readFileAsync(db_FILE, "utf8").then((data) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    let notesArray = JSON.parse(data);
+    notesArray.push(newNote);
+    writeFileAsync(db_FILE, JSON.stringify(notesArray)).then(() => {
+      return res.json(notesArray);
     });
   });
 });
 
-app.delete("/api/notes/:id", function (id) {
+app.delete("/api/notes/:id", function (req, res) {
+  //get the data from the file
+  readFileAsync(db_FILE, "utf8").then((data) => {
+    let notesArray = JSON.parse(data);
+    notesArray = notesArray.filter((note) => note.id !== req.params.id);
+    writeFileAsync(db_FILE, JSON.stringify(notesArray)).then(() => {
+      return res.json(notesArray);
+    });
+  });
+  //parse the data from the file
+  //filter the data to exclude the provided id
+  //save updated (fitered) data to the file
+  //end request (return a success msg)
   readFileAsync(path.join(__dirname, "db", "db.json"), "utf8").then(function (
     notes
   ) {});
